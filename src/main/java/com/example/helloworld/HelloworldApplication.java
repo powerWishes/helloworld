7c5f1a8cd75edf657a77aed2b2fc89eb7bc5d8b1
caosong <caosong1@yonyou.com>
2019-10-12 14:40:41 +0800

    增加日志打印

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -2,20 +2,26 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @RestController
 @SpringBootApplication
+@ServletComponentScan
 public class HelloworldApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(HelloworldApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
     }
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
+        String printStr = "\nhello,\nhello,\nhello.\n";
+        logger.info(printStr);
         return "hello";
     }
 }
